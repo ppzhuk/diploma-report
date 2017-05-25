28cdff3d780aff4983de6e3ab142fe9ac67d123f
Pavel Zhuk <ppzhuk@gmail.com>
2017-05-25 15:02:34 +0300

    impl zd download

---
+++ code/Zendesk.java
@@ -0,0 +1,30 @@
+public class Zendesk implements Closeable {
+    private final AsyncHttpClient client;
+    private final Logger logger;
+	
+    private Integer minRemainingApiCalls;
+    private Integer curRemainingApiCalls = null;
+    private Integer reqInterval;
+
+    // Методы класса опущены
+
+    // Данный метод отвечает за выполнление запроса,
+    // полученного в качестве первого параметра.
+    private <T> ListenableFuture<T> submit(Request request, ZendeskAsyncCompletionHandler<T> handler) {
+        try {
+          // Ожидаем 1 минуту, если количетсво зарпосов слишком велико.
+            if (curRemainingApiCalls != null &&
+                     curRemainingApiCalls < minRemainingApiCalls) {
+                Thread.sleep(60 * 1000);
+            } else {
+                // Иначе ожидаем заданное время.
+                if (reqInterval > 0) {
+                    Thread.sleep(reqInterval);
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new ZendeskException(e.getMessage(), e);
+        }
+        return client.executeRequest(request, handler);
+    }
+}
